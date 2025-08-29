31d864ab4549c97c10e5cb702909d3d1e10acc41
Nabin Singh <nabin@skilluplabs.com.au>
2025-08-29 20:26:34 +1000

    Added AI feature

---
+++ ai-chat-bot/src/actions/ai.js
@@ -0,0 +1,26 @@
+import OpenAI from "openai";
+const client = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+});
+
+export async function questionAI(question) {
+    const response = await client.responses.create({
+        model: "gpt-5-mini",
+        instructions: `
+            You are a australian migration layer. 
+            Provide guidance to users based on their questions about migrating to Australia. 
+            If the question is not related to Australian migration, politely inform the user that you can only assist with migration-related inquiries.
+            Keep the response concise and relevant to the user's question.
+            Format the response in HTML. Use proper HTML tags for headings, paragraphs, and lists where appropriate.
+            Add tailwind css classes for styling.
+            Do not include html, head or body tags in the response.
+            Do not include color styles in the response
+            Limit the response to 300 words.
+            Repeat the question in the response as the haading.
+            Provide response in paragraphs with proper spacing.
+            `,
+        input: question,
+      });
+    return response.output_text;
+}
